f1cd388023aea84189a8e5416639f4b472c73220
Blu <swagnemite@gmx.de>
2016-12-30 02:59:22 +0100

    double check??

--- lua/simfphys/server/mousesteer.lua
+++ lua/simfphys/server/mousesteer.lua
@@ -2,9 +2,6 @@ hook.Add( "VehicleMove", "simfphysmove", function(ply,veh,mv)
 	local vehicle = ply:GetVehicle()
 	if (!vehicle) then return end
 	
-	local vehicle = ply:GetVehicle()
-	if (!vehicle) then return end
-	
 	local vehiclebase = vehicle.vehiclebase
 	
 	if (!IsValid(vehiclebase)) then return end
