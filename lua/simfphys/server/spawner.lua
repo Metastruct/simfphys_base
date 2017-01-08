5ccc6c9a67c1224c80133c1303a125d858734217
Blu <swagnemite@gmx.de>
2017-01-08 15:40:03 +0100

    is this a bug?

--- lua/simfphys/server/spawner.lua
+++ lua/simfphys/server/spawner.lua
@@ -25,12 +25,7 @@ local function MakeLuaVehicle( Player, Pos, Ang, Model, Class, VName, VTable, da
 	Ent.VehicleName = VName
 	Ent.VehicleTable = VTable
 	Ent.EntityOwner = Player
-	
-	timer.Simple( 0.15, function()
-		if (IsValid(Ent)) then
-			Ent:SetSpawn_List( VName )
-		end
-	end)
+	Ent:SetSpawn_List( VName )
 
 	if ( IsValid( Player ) ) then
 		gamemode.Call( "PlayerSpawnedVehicle", Player, Ent )
@@ -79,67 +74,63 @@ function SpawnSimfphysVehicle( Player, vname, tr )
 		table.Merge( Ent, vehicle.Members )
 		duplicator.StoreEntityModifier( Ent, "VehicleMemDupe", vehicle.Members )
 	end
-	
-	timer.Simple( 0.02, function()
-		if (!IsValid(Ent)) then return end
-		
-		if (Ent.ModelInfo) then
-			if (Ent.ModelInfo.Bodygroups) then
-				for i = 1, table.Count( Ent.ModelInfo.Bodygroups ) do
-					Ent:SetBodygroup(i, Ent.ModelInfo.Bodygroups[i] ) 
-				end
-			end
-			
-			if (Ent.ModelInfo.Skin) then
-				Ent:SetSkin( Ent.ModelInfo.Skin )
-			end
-			
-			if (Ent.ModelInfo.Color) then
-				Ent:SetColor( Ent.ModelInfo.Color )
-				
-				local Color = Ent.ModelInfo.Color
-				local dot = Color.r * Color.g * Color.b * Color.a
-				Ent.OldColor = dot
-				
-				local data = {
-					Color = Color,
-					RenderMode = 0,
-					RenderFX = 0
-				}
-				duplicator.StoreEntityModifier( Ent, "colour", data )
+
+	if (Ent.ModelInfo) then
+		if (Ent.ModelInfo.Bodygroups) then
+			for i = 1, table.Count( Ent.ModelInfo.Bodygroups ) do
+				Ent:SetBodygroup(i, Ent.ModelInfo.Bodygroups[i] ) 
 			end
 		end
 		
-		Ent:SetTireSmokeColor(Vector(180,180,180) / 255)
+		if (Ent.ModelInfo.Skin) then
+			Ent:SetSkin( Ent.ModelInfo.Skin )
+		end
 		
-		Ent.Turbocharged = Ent.Turbocharged or false
-		Ent.Supercharged = Ent.Supercharged or false
-		
-		Ent:SetEngineSoundPreset( Ent.EngineSoundPreset )
-		Ent:SetSteerSpeed( Ent.TurnSpeed )
-		Ent:SetMaxTorque( Ent.PeakTorque )
-		Ent:SetDifferentialGear( Ent.DifferentialGear )
-		Ent:SetFastSteerConeFadeSpeed( Ent.SteeringFadeFastSpeed )
-		Ent:SetEfficiency( Ent.Efficiency )
-		Ent:SetMaxTraction( Ent.MaxGrip )
-		Ent:SetTractionBias( Ent.GripOffset / Ent.MaxGrip )
-		Ent:SetPowerDistribution( Ent.PowerBias )
-		
-		Ent:SetBackFire( Ent.Backfire or false )
-		Ent:SetDoNotStall( Ent.DoNotStall or false )
-		
-		Ent:SetIdleRPM( Ent.IdleRPM )
-		Ent:SetLimitRPM( Ent.LimitRPM )
-		Ent:SetRevlimiter( Ent.Revlimiter or false )
-		Ent:SetPowerBandEnd( Ent.PowerbandEnd )
-		Ent:SetPowerBandStart( Ent.PowerbandStart )
-		
-		Ent:SetTurboCharged( Ent.Turbocharged )
-		Ent:SetSuperCharged( Ent.Supercharged )
-		Ent:SetBrakePower( Ent.BrakePower )
-		
-		Ent:SetLights_List( Ent.LightsTable or "no_lights" )
-	end )
+		if (Ent.ModelInfo.Color) then
+			Ent:SetColor( Ent.ModelInfo.Color )
+			
+			local Color = Ent.ModelInfo.Color
+			local dot = Color.r * Color.g * Color.b * Color.a
+			Ent.OldColor = dot
+			
+			local data = {
+				Color = Color,
+				RenderMode = 0,
+				RenderFX = 0
+			}
+			duplicator.StoreEntityModifier( Ent, "colour", data )
+		end
+	end
+	
+	Ent:SetTireSmokeColor(Vector(180,180,180) / 255)
+	
+	Ent.Turbocharged = Ent.Turbocharged or false
+	Ent.Supercharged = Ent.Supercharged or false
+	
+	Ent:SetEngineSoundPreset( Ent.EngineSoundPreset )
+	Ent:SetSteerSpeed( Ent.TurnSpeed )
+	Ent:SetMaxTorque( Ent.PeakTorque )
+	Ent:SetDifferentialGear( Ent.DifferentialGear )
+	Ent:SetFastSteerConeFadeSpeed( Ent.SteeringFadeFastSpeed )
+	Ent:SetEfficiency( Ent.Efficiency )
+	Ent:SetMaxTraction( Ent.MaxGrip )
+	Ent:SetTractionBias( Ent.GripOffset / Ent.MaxGrip )
+	Ent:SetPowerDistribution( Ent.PowerBias )
+	
+	Ent:SetBackFire( Ent.Backfire or false )
+	Ent:SetDoNotStall( Ent.DoNotStall or false )
+	
+	Ent:SetIdleRPM( Ent.IdleRPM )
+	Ent:SetLimitRPM( Ent.LimitRPM )
+	Ent:SetRevlimiter( Ent.Revlimiter or false )
+	Ent:SetPowerBandEnd( Ent.PowerbandEnd )
+	Ent:SetPowerBandStart( Ent.PowerbandStart )
+	
+	Ent:SetTurboCharged( Ent.Turbocharged )
+	Ent:SetSuperCharged( Ent.Supercharged )
+	Ent:SetBrakePower( Ent.BrakePower )
+	
+	Ent:SetLights_List( Ent.LightsTable or "no_lights" )
 
 	undo.Create( "Vehicle" )
 		undo.SetPlayer( Player )
