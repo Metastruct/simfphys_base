72e99f37668179b6b89bcd827cb450849aeacdaf
Blu <swagnemite@gmx.de>
2017-01-28 13:13:57 +0100

    added convar to disable forced workshop downloading

--- lua/autorun/simfphys_init.lua
+++ lua/autorun/simfphys_init.lua
@@ -1,2 +1,10 @@
 AddCSLuaFile("simfphys/init.lua")
 include("simfphys/init.lua")
+
+local ForceWorkshop = CreateConVar("sv_simfphys_forceworkshop", 1, {FCVAR_ARCHIVE}, "Force clients to download the content from the workshop? (requires a restart)")
+
+if SERVER then
+	if ForceWorkshop:GetBool() then
+		resource.AddWorkshop("771487490")
+	end
+end
--- lua/simfphys/server/resource.lua
+++
@@ -1,1 +0,0 @@
-resource.AddWorkshop("771487490")
