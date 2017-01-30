c39d7a1e391aade24900b3cc086fefd5acefd2b1
Blu <swagnemite@gmx.de>
2017-01-30 12:38:26 +0100

    oh man this script is disgusting, but hey it works. Maybe needs an rewrite when i have time Lol

--- lua/simfphys/server/exitpoints.lua
+++ lua/simfphys/server/exitpoints.lua
@@ -1,14 +1,16 @@
 --special thanks to SligWolf for helping me with this one
 
-local function Exit_vehicle_simple( ent, ply, b_ent )
-
-	if !IsValid(ent) then return end
-	if !IsValid(ply) then return end
-	if !IsValid(b_ent) then return end
-	
+local function ExitUsingMyTraces( ent, ply, b_ent )
+	local Center = b_ent:LocalToWorld( b_ent:OBBCenter() )
 	local vel = b_ent:GetVelocity()
 	local radius = b_ent:BoundingRadius()
 	local HullSize = Vector(18,18,0)
+	local Filter1 = {ent,ply}
+	local Filter2 = {ent,ply,b_ent}
+	for i = 1, table.Count( b_ent.Wheels ) do
+		table.insert(Filter1, b_ent.Wheels[i])
+		table.insert(Filter2, b_ent.Wheels[i])
+	end
 	
 	if (vel:Length() > 250) then
 		local pos = b_ent:GetPos()
@@ -20,7 +22,7 @@ local function Exit_vehicle_simple( ent, ply, b_ent )
 			endpos = targetpos - Vector(0,0,10),
 			maxs = HullSize,
 			mins = -HullSize,
-			filter = {}
+			filter = Filter1
 		} )
 		local exitpoint = tr.HitPos + Vector(0,0,10)
 		
@@ -40,10 +42,12 @@ local function Exit_vehicle_simple( ent, ply, b_ent )
 			endpos = targetpos + Vector(0,0,80),
 			maxs = HullSize,
 			mins = -HullSize,
-			filter = {ent,ply}
+			filter = Filter1
 		} )
+		local traceto = util.TraceLine( {start = Center,endpos = targetpos,filter = Filter2} )
+		
 		local HitGround = tr1.Hit
-		local HitWall = tr2.Hit
+		local HitWall = tr2.Hit or traceto.Hit
 		
 		local check0 = (HitWall == true or HitGround == false or util.IsInWorld( targetpos ) == false) and (pos - ent:GetRight() * 80) or targetpos
 		local tr = util.TraceHull( {
@@ -51,9 +55,10 @@ local function Exit_vehicle_simple( ent, ply, b_ent )
 			endpos = check0 + Vector(0,0,80),
 			maxs = HullSize,
 			mins = -HullSize,
-			filter = {ent,ply}
+			filter = Filter1
 		} )
-		local HitWall = tr.Hit
+		local traceto = util.TraceLine( {start = Center,endpos = check0,filter = Filter2} )
+		local HitWall = tr.Hit or traceto.hit
 		
 		local check1 = (HitWall == true or HitGround == false or util.IsInWorld( check0 ) == false) and (pos + ent:GetUp() * 100) or check0
 		
@@ -62,9 +67,10 @@ local function Exit_vehicle_simple( ent, ply, b_ent )
 			endpos = check1 + Vector(0,0,80),
 			maxs = HullSize,
 			mins = -HullSize,
-			filter = {ent,ply}
+			filter = Filter1
 		} )
-		local HitWall = tr.Hit
+		local traceto = util.TraceLine( {start = Center,endpos = check1,filter = Filter2} )
+		local HitWall = tr.Hit or traceto.hit
 		local check2 = (HitWall == true or util.IsInWorld( check1 ) == false) and (pos - ent:GetUp() * 100) or check1
 		
 		local tr = util.TraceHull( {
@@ -72,9 +78,10 @@ local function Exit_vehicle_simple( ent, ply, b_ent )
 			endpos = check2 + Vector(0,0,80),
 			maxs = HullSize,
 			mins = -HullSize,
-			filter = {ent,ply}
+			filter = Filter1
 		} )
-		local HitWall = tr.Hit
+		local traceto = util.TraceLine( {start = Center,endpos = check2,filter = Filter2} )
+		local HitWall = tr.Hit or traceto.hit
 		local check3 = (HitWall == true or util.IsInWorld( check2 ) == false) and b_ent:LocalToWorld( Vector(0,radius,0) ) or check2
 		
 		local tr = util.TraceHull( {
@@ -82,9 +89,10 @@ local function Exit_vehicle_simple( ent, ply, b_ent )
 			endpos = check3 + Vector(0,0,80),
 			maxs = HullSize,
 			mins = -HullSize,
-			filter = {ent,ply}
+			filter = Filter1
 		} )
-		local HitWall = tr.Hit
+		local traceto = util.TraceLine( {start = Center,endpos = check3,filter = Filter2} )
+		local HitWall = tr.Hit or traceto.hit
 		local check4 = (HitWall == true or util.IsInWorld( check3 ) == false) and b_ent:LocalToWorld( Vector(0,-radius,0) ) or check3
 		
 		local tr = util.TraceHull( {
@@ -92,13 +100,63 @@ local function Exit_vehicle_simple( ent, ply, b_ent )
 			endpos = check4 + Vector(0,0,80),
 			maxs = HullSize,
 			mins = -HullSize,
-			filter = {ent,ply}
+			filter = Filter1
 		} )
-		local HitWall = tr.Hit
+		local traceto = util.TraceLine( {start = Center,endpos = check4,filter = Filter2} )
+		local HitWall = tr.Hit or traceto.hit
 		local exitpoint = (HitWall == true or util.IsInWorld( check4 ) == false) and b_ent:LocalToWorld( Vector(0,0,0) ) or check4
 		
 		ply:SetPos(exitpoint)
 		ply:SetEyeAngles((pos - exitpoint):Angle())
+	end
+end
+
+local function ExitUsingAttachments( ent, ply, b_ent )
+	local Center = b_ent:LocalToWorld( b_ent:OBBCenter() )
+	local Filter = {ent,ply,b_ent}
+	
+	for i = 1, table.Count( b_ent.Wheels ) do
+		table.insert(Filter, b_ent.Wheels[i])
+	end
+	
+	for i = 1, table.Count( b_ent.Exitpoints ) do
+		local seq_att = b_ent.Exitpoints[i]
+		local attachmentdata = b_ent:GetAttachment( b_ent:LookupAttachment( seq_att ) )
+		local targetpos = attachmentdata.Pos
+		local targetang = attachmentdata.Ang
+		targetang.r = 0
+		
+		local tr = util.TraceLine( {
+			start = Center,
+			endpos = targetpos,
+			filter = Filter
+		} )
+		local Hit = tr.Hit
+		local InWorld = util.IsInWorld( targetpos )
+		local IsBlocked = Hit or !InWorld
+		
+		if !IsBlocked then
+			ply:SetPos( targetpos )
+			ply:SetEyeAngles( targetang )
+			b_ent:PlayAnimation( seq_att )
+			
+			return
+		end
+	end
+	
+	ExitUsingMyTraces( ent, ply, b_ent )
+end
+
+local function Exit_vehicle_simple( ent, ply, b_ent )
+
+	if !IsValid(ent) then return end
+	if !IsValid(ply) then return end
+	if !IsValid(b_ent) then return end
+	
+	if istable( b_ent.Exitpoints ) and b_ent:GetVelocity():Length() < 250 then
+		ExitUsingAttachments( ent, ply, b_ent )
+	else
+		ExitUsingMyTraces( ent, ply, b_ent )
 	end
 end
 
