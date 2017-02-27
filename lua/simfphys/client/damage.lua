789f4542056c9e2b92ee4f80aafa938aca373016
Python1320 <Python1320@users.noreply.github.com>
2017-02-27 23:48:28 +0200

    Update damage.lua

--- lua/simfphys/client/damage.lua
+++ lua/simfphys/client/damage.lua
@@ -1,6 +1,6 @@
 local function damagedbackfire( length )
 	local veh = net.ReadEntity()
-	if !IsValid(veh) then return end
+	if not veh:IsValid() or not veh.Backfire then return end
 	veh:Backfire( true )
 end
 net.Receive("simfphys_backfire", damagedbackfire)
