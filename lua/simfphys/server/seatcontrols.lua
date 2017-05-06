a4ac8a382f237f1a3ede09bed053d9c5ef8c0030
Blu <swagnemite@gmx.de>
2017-05-06 13:11:56 +0200

    seat switching anim fix

--- lua/simfphys/server/seatcontrols.lua
+++ lua/simfphys/server/seatcontrols.lua
@@ -15,25 +15,27 @@ local function handleseatswitching( length, ply )
 	local ply = net.ReadEntity()
 	local req_seat = net.ReadInt( 32 )
 	
-	if !IsValid(vehicle) then return end
-	if !IsValid(ply) then return end
+	if not IsValid( vehicle ) then return end
+	if not IsValid( ply ) then return end
 	
 	ply.NextSeatSwitch = ply.NextSeatSwitch or 0
 	
-	if (ply.NextSeatSwitch < CurTime()) then
+	if ply.NextSeatSwitch < CurTime() then
 		ply.NextSeatSwitch = CurTime() + 0.5
 		
-		if (req_seat == 0) then
-			if !IsValid(vehicle:GetDriver()) then
+		if req_seat == 0 then
+			if not IsValid( vehicle:GetDriver() ) then
 				ply:ExitVehicle()
 				
-				if (IsValid(vehicle.DriverSeat)) then
+				if IsValid(vehicle.DriverSeat) then
 					timer.Simple( 0.05, function()
-						if !IsValid(vehicle) then return end
-						if !IsValid(ply) then return end
+						if not IsValid(vehicle) then return end
+						if not IsValid(ply) then return end
 						if IsValid(vehicle:GetDriver()) then return end
 						
 						ply:EnterVehicle( vehicle.DriverSeat )
+						vehicle:EnteringSequence( ply )
+						
 						ply:SetAllowWeaponsInVehicle( false ) 
 						local angles = Angle(0,90,0)
 						ply:SetEyeAngles( angles )
@@ -41,15 +43,15 @@ local function handleseatswitching( length, ply )
 				end
 			end
 		else
-			if (!vehicle.pSeat) then return end
+			if not vehicle.pSeat then return end
 			local seat = vehicle.pSeat[req_seat]
-			if (IsValid(seat) and !IsValid(seat:GetDriver())) then
+			if IsValid(seat) and not IsValid( seat:GetDriver() ) then
 				ply:ExitVehicle()
 				
 				timer.Simple( 0.05, function()
-					if !IsValid(vehicle) then return end
-					if !IsValid(ply) then return end
-					if IsValid(seat:GetDriver()) then return end
+					if not IsValid( vehicle ) then return end
+					if not IsValid( ply ) then return end
+					if IsValid( seat:GetDriver() ) then return end
 					
 					ply:EnterVehicle( seat )
 					local angles = Angle(0,90,0)
