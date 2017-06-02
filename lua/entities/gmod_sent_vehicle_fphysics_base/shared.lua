b5b33a8912558aef462ce1fec40a48ee351ead08
Zeni <ToastyEngineer@users.noreply.github.com>
2017-06-02 18:18:35 +0200

    Fix bad

--- lua/entities/gmod_sent_vehicle_fphysics_base/shared.lua
+++ lua/entities/gmod_sent_vehicle_fphysics_base/shared.lua
@@ -118,10 +118,3 @@ function ENT:BodyGroupIsValid( bodygroups )
 	return false
 end
 
-function ENT:CanProperty( ply, property )
-	local owner = self.CPPIGetOwner and self:CPPIGetOwner() or NULL
-	if (not IsValid(owner) or ply.AreFriends and not ply:AreFriends(owner)) and property == "editentity" then
-		return false
-	end
-end
-		
