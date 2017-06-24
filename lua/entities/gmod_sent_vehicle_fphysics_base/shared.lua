665cc4c6c6cee0f63b745cb4313da56f261b7e1c
Blu <swagnemite@gmx.de>
2017-06-24 15:36:53 +0200

    lets change the default values

--- lua/entities/gmod_sent_vehicle_fphysics_base/shared.lua
+++ lua/entities/gmod_sent_vehicle_fphysics_base/shared.lua
@@ -9,7 +9,7 @@ ENT.Spawnable       = false
 ENT.AdminSpawnable  = false
 
 ENT.AutomaticFrameAdvance = true
-ENT.RenderGroup = RENDERGROUP_BOTH
+ENT.RenderGroup = RENDERGROUP_BOTH 
 ENT.Editable = (GetConVar("sv_simfphys_devmode"):GetInt() or 1) >= 1
 ENT.IsSimfphyscar = true
 
@@ -85,11 +85,23 @@ function VehicleMeta:IsVehicle()
 end
 
 function ENT:GetCurHealth()
-	return self:GetNWFloat( "Health", 0 )
+	return self:GetNWFloat( "Health", self:GetMaxHealth() )
 end
 
 function ENT:GetMaxHealth()
-	return self:GetNWFloat( "MaxHealth", 0 )
+	return self:GetNWFloat( "MaxHealth", 2000 )
+end
+
+function ENT:GetMaxFuel()
+	return self:GetNWFloat( "MaxFuel", 60 )
+end
+
+function ENT:GetFuel()
+	return self:GetNWFloat( "Fuel", self:GetMaxFuel() )
+end
+
+function ENT:GetFuelUse()
+	return self:GetNWFloat( "FuelUse", 0 )
 end
 
 function ENT:OnSmoke()
