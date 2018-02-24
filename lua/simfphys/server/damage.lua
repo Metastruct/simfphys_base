e4943f9494812a0354c2cae932c04b504ef4cfe2
Blu <swagnemite@gmx.de>
2018-02-25 00:21:19 +0100

    added NoWheelGibs variable
    servercrash fix for armed vehicles pack

--- lua/simfphys/server/damage.lua
+++ lua/simfphys/server/damage.lua
@@ -56,7 +56,7 @@ local function DestroyVehicle( ent )
 		ply:AddCleanup( "Gibs", bprop )
 	end
 	
-	if ent.CustomWheels == true then
+	if ent.CustomWheels == true and not ent.NoWheelGibs then
 		for i = 1, table.Count( ent.GhostWheels ) do
 			local Wheel = ent.GhostWheels[i]
 			if IsValid(Wheel) then
